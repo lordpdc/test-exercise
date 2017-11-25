0d2d4ecbec65406b04802592b95a34b975ff032c
Roman Ramirez <lordpdc@hotmail.com>
2017-11-25 12:38:33 -0600

    first exercise

---
+++ helloWorld/src/Greeter.java
@@ -0,0 +1,10 @@
+public class Greeter {
+
+    public static void main(String[] args) {
+        // Write your code here
+        // Please print "Hello world!"
+        // Plese print "(And all the people of the world)"
+       
+    }
+
+}
