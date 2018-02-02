64554b6f9006d2dbc1a8a58ba452c5618909ed09
Roman Ramirez <lordpdc@hotmail.com>
2018-02-02 14:05:22 -0600

    c

--- 023_Temperatures/src/Temperatures.java
+++ 023_Temperatures/src/Temperatures.java
@@ -6,6 +6,7 @@ public class Temperatures {
     public static void main(String[] args) {
 
         Scanner reader = new Scanner(System.in);
+        
         // Write your code here. 
         //BEGIN SOLUTION
         while (true){
@@ -16,13 +17,5 @@ public class Temperatures {
             Graph.addNumber(number);
         }
         //END SOLUTION
-        //STUB: //while the number are betwen -29 and 39 add the numbre to graphic whith Graph class
-        //STUB: Graph is used as follows:
-        //STUB: Graph.addNumber(7);
-        //STUB: double value = 13.5;
-        //STUB: Graph.addNumber(value);
-        //STUB: value = 3;
-        //STUB: Graph.addNumber(value);
-        //STUB: Remove or comment out these lines above before trying to run the tests.
     }
 }
